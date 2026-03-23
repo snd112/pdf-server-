8580467ecc2a3534c0e981d0f7d603ac33201f9a
snd112 <snd555890@gmail.com>
2026-03-23 22:25:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ app.get("/", (req, res) => {
 });
 
 // ================= START =================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT 
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Running on port", PORT);
